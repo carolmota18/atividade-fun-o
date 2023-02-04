a7ce92e72e738653d793f0e1042dc4d7894988d6
carolmota18 <carolinemota78@gmail.com>
2023-02-03 23:04:19 -0300

    codigo finalizado

---
+++ script.js
@@ -0,0 +1,43 @@
+function teste(estafuncao){
+    console.log(estafuncao);
+}
+
+teste("tudo certo")
+
+function mostrarNome(nome){
+console.log(nome);
+}
+mostrarNome("caroline")
+mostrarNome("joao")
+
+function sobreMim(nome,idade,estilomusical){
+    console.log(nome,idade,estilomusical);
+
+}
+sobreMim("Caroline",22,"Pop")
+
+function gostos(filme,musica){
+    console.log(filme,musica)
+}
+gostos("as branquelas","telephone")
+
+function triplo(numero){
+    console.log(numero)
+
+}
+triplo(3*3)
+
+
+let idade= 22;
+
+function verificarIdade(){
+    if (idade > 18){
+        return console.log("você pode ver esse filme");
+    } else (idade < 18);{
+        return console.log("você não pode ver esse filme");
+    }
+
+    }
+   
+
+verificarIdade();
